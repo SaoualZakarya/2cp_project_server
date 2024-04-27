417070eb10ef81d2b83a4c282264d8f146e5ff0a
zakaryasaoual@gmail.com <zakaryasaoual@gmail.com>
2024-04-27 18:31:40 +0100

    add the notification

--- routes/notifcation.js
+++ routes/notifcation.js
@@ -2,15 +2,12 @@ import express from 'express'
 import {authMiddleware,isBlocked,isVerified} from '../middlewares/authMiddleware.js'
 import notification from '../controllers/notification.js';
 
-
 const router = express.Router()
 
-
-
-
-
-
-
+// get all notificaton for the user 
+router.get('/all',authMiddleware,isBlocked,isVerified,notification.getAllNotification)
+// update notificaton status 
+router.put('/:id',authMiddleware,isBlocked,isVerified,notification.checkNotification)
 
 
 export default router ;
