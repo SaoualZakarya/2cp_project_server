16f1b14b6696b2a0d79ea5c19765f4f072481af2
zakaryasaoual@gmail.com <zakaryasaoual@gmail.com>
2024-05-02 18:58:39 +0100

    fix problems

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 // middlewares
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}))
+// app.use(cors({credentials: true, origin: 'http://localhost:5173'}))
 app.use(express.json({limit: '100mb'}))
 app.use(express.urlencoded({limit: '100mb', extended: false}))
 app.use(cookieParser())
